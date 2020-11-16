c4e81aa5ec8857faed71023f0975b93c3565c076
Carlos Cabas <ing.carloscabas@gmail.com>
2020-11-16 14:37:03 -0500

    funcionalidad de calculo de áreas terminada

--- app/src/main/java/com/example/vistas/Calculo.java
+++ app/src/main/java/com/example/vistas/Calculo.java
@@ -2,8 +2,10 @@ package com.example.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -14,6 +16,7 @@ public class Calculo extends AppCompatActivity {
 
     private TextView titulo;
     private LinearLayout content;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,6 @@ public class Calculo extends AppCompatActivity {
 
         titulo = findViewById(R.id.txtTituloCalculo);
         content = (LinearLayout)findViewById(R.id.lytCalculo);
-
 
         Bundle b = getIntent().getExtras();
         int value = b.getInt("param");
@@ -39,6 +41,17 @@ public class Calculo extends AppCompatActivity {
         lytButtons.addView(btnCalcular);
         lytButtons.addView(btnBorrar);
 
+        intent = new Intent(Calculo.this, Resultado.class);
+        Bundle param = new Bundle();
+        param.putInt("show", 0);
+
+        EditText txtValue = new EditText(this);
+        EditText txtBase = new EditText(this);
+        EditText txtAltura = new EditText(this);
+        EditText txtBaseT = new EditText(this);
+        EditText txtAlturaT = new EditText(this);
+        EditText txtRadio = new EditText(this);
+
         switch (value){
             case 0: //cuadrado
                 titulo.setText(getResources().getString(R.string.area1).toUpperCase());
@@ -46,11 +59,29 @@ public class Calculo extends AppCompatActivity {
                 label.setGravity(Gravity.CENTER_VERTICAL);
                 label.setText(getResources().getString(R.string.lblcuadrado));
 
-                EditText txtValue = new EditText(this);
                 txtValue.setGravity(Gravity.CENTER_VERTICAL);
 
                 content.addView(label);
                 content.addView(txtValue);
+
+                btnCalcular.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(txtValue.getText().toString().isEmpty()){
+                            txtValue.setError(getResources().getString(R.string.errcuadrado));
+                            txtValue.requestFocus();
+                        }
+                        else {
+                            double lado = Double.parseDouble(txtValue.getText().toString());
+                            double area = lado * lado;
+                            param.putDouble("area", area);
+                            param.putString("title", getResources().getString(R.string.area1).toUpperCase());
+                            intent.putExtras(param);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
                 break;
 
             case 1:
@@ -63,16 +94,37 @@ public class Calculo extends AppCompatActivity {
                 lblaltura.setGravity(Gravity.CENTER_VERTICAL);
                 lblaltura.setText(getResources().getString(R.string.lblaltura));
 
-                EditText txtBase = new EditText(this);
                 txtBase.setGravity(Gravity.CENTER_VERTICAL);
-
-                EditText txtAltura = new EditText(this);
                 txtAltura.setGravity(Gravity.CENTER_VERTICAL);
 
                 content.addView(lblbase);
                 content.addView(txtBase);
                 content.addView(lblaltura);
                 content.addView(txtAltura);
+
+                btnCalcular.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(txtBase.getText().toString().isEmpty()){
+                            txtBase.setError(getResources().getString(R.string.errcuadrado));
+                            txtBase.requestFocus();
+                        }
+                        else if(txtAltura.getText().toString().isEmpty()){
+                            txtAltura.setError(getResources().getString(R.string.errcuadrado));
+                            txtAltura.requestFocus();
+                        }
+                        else {
+                            double base = Double.parseDouble(txtBase.getText().toString());
+                            double altura = Double.parseDouble((txtAltura.getText().toString()));
+                            double area = base * altura;
+                            param.putDouble("area", area);
+                            param.putString("title", getResources().getString(R.string.area2).toUpperCase());
+                            intent.putExtras(param);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
                 break;
 
             case 2:
@@ -85,16 +137,38 @@ public class Calculo extends AppCompatActivity {
                 lblalturaT.setGravity(Gravity.CENTER_VERTICAL);
                 lblalturaT.setText(getResources().getString(R.string.lblaltura));
 
-                EditText txtBaseT = new EditText(this);
-                txtBaseT.setGravity(Gravity.CENTER_VERTICAL);
 
-                EditText txtAlturaT = new EditText(this);
+                txtBaseT.setGravity(Gravity.CENTER_VERTICAL);
                 txtAlturaT.setGravity(Gravity.CENTER_VERTICAL);
 
                 content.addView(lblbaseT);
                 content.addView(txtBaseT);
                 content.addView(lblalturaT);
                 content.addView(txtAlturaT);
+
+                btnCalcular.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(txtBaseT.getText().toString().isEmpty()){
+                            txtBaseT.setError(getResources().getString(R.string.errcuadrado));
+                            txtBaseT.requestFocus();
+                        }
+                        else if(txtAlturaT.getText().toString().isEmpty()){
+                            txtAlturaT.setError(getResources().getString(R.string.errcuadrado));
+                            txtAlturaT.requestFocus();
+                        }
+                        else {
+                            double base = Double.parseDouble(txtBaseT.getText().toString());
+                            double altura = Double.parseDouble(txtAlturaT.getText().toString());
+                            double area = base * altura / 2;
+                            param.putDouble("area", area);
+                            param.putString("title", getResources().getString(R.string.area3).toUpperCase());
+                            intent.putExtras(param);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
                 break;
 
             case 3:
@@ -103,14 +177,45 @@ public class Calculo extends AppCompatActivity {
                 lblradio.setGravity(Gravity.CENTER_VERTICAL);
                 lblradio.setText(getResources().getString(R.string.lblradio));
 
-                EditText txtRadio = new EditText(this);
                 txtRadio.setGravity(Gravity.CENTER_VERTICAL);
 
                 content.addView(lblradio);
                 content.addView(txtRadio);
+
+                btnCalcular.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(txtRadio.getText().toString().isEmpty()){
+                            txtRadio.setError(getResources().getString(R.string.errcuadrado));
+                            txtRadio.requestFocus();
+                        }
+                        else {
+                            double radio = Double.parseDouble(txtRadio.getText().toString());
+                            double area = Math.PI * (radio * radio);
+                            param.putDouble("area", area);
+                            param.putString("title", getResources().getString(R.string.area4).toUpperCase());
+                            intent.putExtras(param);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
                 break;
         }
 
         content.addView(lytButtons);
+
+        btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtValue.setText("");
+                txtBase.setText("");
+                txtAltura.setText("");
+                txtBaseT.setText("");
+                txtAlturaT.setText("");
+                txtRadio.setText("");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/vistas/Resultado.java
+++ app/src/main/java/com/example/vistas/Resultado.java
@@ -2,13 +2,43 @@ package com.example.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
+
+    private TextView titulo;
+    private TextView lblResultado;
+    private TextView lblResultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        titulo = findViewById(R.id.lblTituloResultado);
+        lblResultado = findViewById(R.id.lblResultadoValue);
+        lblResultText = findViewById(R.id.lblResultado);
+
+        Bundle b = getIntent().getExtras();
+        double area = b.getDouble("area");
+        String title = b.getString("title");
+        int show = b.getInt("show");
+
+        if(show == 0){
+            lblResultText.setText(R.string.lblResultadoArea);
+        }else{
+            lblResultText.setText(R.string.lblResultadoVolume);
+        }
+        titulo.setText(title);
+        lblResultado.setText(Double.toString(area));
+
+    }
+
+    public void goback(View v){
+        Intent intent = new Intent(Resultado.this, MainActivity.class);
+        startActivity(intent);
     }
 }
